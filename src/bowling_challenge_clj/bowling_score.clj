0894fa8537b8f3fb36b1a85c7747ace6e220bcbb
Deon Tan <deonjtan@gmail.com>
2015-11-24 14:36:22 +0000

    'bowling-score' fixed for edge case of latest score ending in spare/strike

--- src/bowling_challenge_clj/bowling_score.clj
+++ src/bowling_challenge_clj/bowling_score.clj
@@ -5,21 +5,28 @@
      10))
 
 (defn spare? [frame]
-  (and 
+  (and
     (not (strike? frame))
     (= (reduce + frame)
        10)))
 
 (defn next-2-rolls [rolls]
-  (list (first rolls)
-    (first (rest rolls))))
+  (cond
+    (empty? rolls)
+      '(0)
+    (= (count rolls) 1)
+      (let [first-bonus (first rolls)]
+      (list (first rolls))
+    :else (cons first-bonus (first (rest rolls))))))
 
 (defn strike-score [scorecard]
   (+ (first (first scorecard))
     (reduce + (next-2-rolls (rest (flatten scorecard))))))
 
 (defn next-roll [scorecard]
-  (first (first scorecard)))
+  (if (empty? scorecard)
+    0
+    (first (first scorecard))))
 
 (defn spare-score [scorecard]
   (+ (reduce + (first scorecard))
@@ -27,7 +34,7 @@
 
 (defn frame-score [scorecard]
   (let [frame (first scorecard)]
-    (cond 
+    (cond
      (strike? frame)
        (strike-score scorecard)
      (spare? frame)
